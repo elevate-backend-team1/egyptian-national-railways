d51e545663cae8fc2c9c42c8369acf8d6568b836
RamyElgharbawy <ra.elgharbawy@gmail.com>
2025-12-03 18:45:03 +0200

    setup linting rules and github actions integrated with google ts style guide

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/egyptian-railways'),],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/egyptian-railways')],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
-
-
-
-
 export class AppModule {}
