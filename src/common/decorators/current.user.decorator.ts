694e221c0051b7b855e6ab9638dba8e6a26611e7
emangomaa <emangomaa149@gmail.com>
2025-12-20 19:21:25 +0200

    edit user decerator

--- src/common/decorators/current.user.decorator.ts
+++ src/common/decorators/current.user.decorator.ts
@@ -1,9 +1,9 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import type { Request } from 'express';
+import { JwtPayload } from 'src/modules/auth/strategies/jwt.strategy';
 
 export const CurrentUser = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest<Request>();
-    return (request.user as any).userId;
+    const request = ctx.switchToHttp().getRequest<{ user: JwtPayload }>();
+    return request.user.sub;
   },
-);
+);  
