f4cf8c42a8062466a4b870a65b422c216b397d86
Abdullah42021231 <bda163587@gmail.com>
2025-12-24 16:45:22 +0200

    fix lint

--- src/modules/payment/payment.controller.ts
+++ src/modules/payment/payment.controller.ts
@@ -2,6 +2,13 @@ import { Controller, Post, Body, Get, UseGuards, Req } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { CreateCardDto } from './dto/create-card.dto';
 import { JwtAuthGuard } from '../../common/guards/jwt-auth.guard';
+import { Request } from 'express';
+
+interface AuthRequest extends Request {
+  user: {
+    id: string;
+  };
+}
 
 @Controller('payment')
 @UseGuards(JwtAuthGuard)
@@ -9,13 +16,13 @@ export class PaymentController {
   constructor(private readonly paymentService: PaymentService) {}
 
   @Post('cards')
-  async addCard(@Req() req: any, @Body() dto: CreateCardDto) {
+  async addCard(@Req() req: AuthRequest, @Body() dto: CreateCardDto) {
     const userId = req.user.id;
     return this.paymentService.addCard(userId, dto);
   }
 
   @Get('cards')
-  async getMyCards(@Req() req: any) {
+  async getMyCards(@Req() req: AuthRequest) {
     const userId = req.user.id;
     return this.paymentService.listCards(userId);
   }
